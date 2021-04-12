a8c208bf9db6bc46ee9e25f4a67fdf6a5192e17a
AGuerand <73344943+AGuerand@users.noreply.github.com>
2021-04-12 14:05:41 +0200

    Add files via upload

---
+++ Insult.py
@@ -0,0 +1,9 @@
+class Insult:
+    def __init__(self):
+        self.insultFinal=["you six piece chicken Mcnobody", "hope you get hit by a truck an survive", "like a gta character", "you meth-head", "you donut", "with your yeeyee *** haricut", "you man child", "you precious stuck up bi... du-dude", "and remember your poor bruh", "you absolute cretin", "you fool", "hope a creeper blow up your house", "hope you step on a lego"]
+        self.sujet=["your hair", "your dog", "you","your car", "your job", "your cat", "your MOM !", "your sister", "your brother", "your uncle", "your aunt", "your family", "your hat", "your outfit", "your face", "your bank account", "your beard"]
+        self.starter=["looks like nabil", "should", "smells like", "act like", "you should know that","I hope that"]
+        self.complement=["tomorow", "today", "street", "hospital", "this morning", "tonight", "in a fight", "your room", "in the kitchen"]
+        self.liaisons=["so","if","almost","then","it","like","a", "you"]
+        self.verb=["is", "will be", "eating", "eat", "fight","walking","walk","sleeping","sleep","fall","falling","move","moving"] 
+        self.adjectives=["fat","ugly","sad","redneck","french","retarded","blind","dumb","stupid","lonely","villain","obese","bad","crazy","childish","beautiful", "happpppppppppppppppppppppy","deppresed","prisoner","clown"]
---
+++ Role.py
@@ -0,0 +1,25 @@
+class character:
+    def __init__(self,role):
+        self.role = role
+        if role == "The":
+            self.strenght = "style"
+            self.weakness = "family"
+            self.point = 0
+        if role == "The rich dude":
+            self.strenght = ""
+            self.weakness = ""
+            self.point = 100
+        if role == "The granny"
+            self.strenght = ""
+            self.weakness = ""
+            self.point = 0
+        if role == "The npc"
+            self.strenght = ""
+            self.weakness = ""
+            self.point = 0
+
+    def __str__(self):
+        return self.role
+
+    def Achat(self,liste_achat):
+        self.achats.append(liste_achat)
