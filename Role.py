45c04ea6607291c1d3a08b2715e6e2984b50446d
AGuerand <anthony.guerand@ynov.com>
2021-04-12 14:09:42 +0200

    first commit

--- Role.py
+++ Role.py
@@ -1,7 +1,7 @@
 class character:
     def __init__(self,role):
         self.role = role
-        if role == "The":
+        if role == "The nice dude":
             self.strenght = "style"
             self.weakness = "family"
             self.point = 0
@@ -16,6 +16,10 @@ class character:
         if role == "The npc"
             self.strenght = ""
             self.weakness = ""
+            self.point = 0
+        if role == "Jeff":
+            self.strenght = "style"
+            self.weakness = "family"
             self.point = 0
 
     def __str__(self):
